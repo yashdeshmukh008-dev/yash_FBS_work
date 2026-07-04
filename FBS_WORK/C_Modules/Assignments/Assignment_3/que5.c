590754a4719bc0f43c025b55a47cb052300bc112
yashdeshmukh008-dev <yashdeshmukh008@gmail.com>
2026-07-04 16:18:22 +0530

    Create que5.c

---
+++ FBS_WORK/C_Modules/Assignments/Assignment_3/que5.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+
+void main() {
+    int num, originalNum, remainder, digits = 0;
+    int result = 0;
+    num = 153;
+    originalNum = num;
+    while (originalNum != 0) {
+        originalNum /= 10;
+        digits++;
+    }
+    originalNum = num;
+    while (originalNum != 0) {
+        remainder = originalNum % 10;
+        result += pow(remainder, digits);
+        originalNum /= 10;
+    }
+    if (result == num)
+        printf("%d is an Armstrong number.", num);
+    else
+        printf("%d is not an Armstrong number.", num);
+}
