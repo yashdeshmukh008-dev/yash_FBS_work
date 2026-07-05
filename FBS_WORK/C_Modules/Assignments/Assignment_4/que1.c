d8c8f435287688028c780264bab128f8bf619dfb
yashdeshmukh008-dev <yashdeshmukh008@gmail.com>
2026-07-05 22:29:16 +0530

    Create que1.c

---
+++ FBS_WORK/C_Modules/Assignments/Assignment_4/que1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+
+ void main() {
+    int n = 1000; 
+    int num, temp, rem, digits, sum;
+
+    printf("Armstrong numbers between 1 and %d are:\n", n);
+
+    for (num = 1; num <= n; num++) {
+        temp = num;
+        digits = 0;
+        sum = 0;
+        while (temp != 0) {
+            digits++;
+            temp /= 10;
+        }
+
+        temp = num;
+        while (temp != 0) {
+            rem = temp % 10;
+            sum += pow(rem, digits);
+            temp /= 10;
+        }
+        if (sum == num)
+            printf("%d ", num);
+    }
+
+    
+}
