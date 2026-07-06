61aba64a41789cb9d0ccbe160f3ba706e530b1f0
yashdeshmukh008-dev <yashdeshmukh008@gmail.com>
2026-07-06 16:41:08 +0530

    Create que8.c

---
+++ FBS_WORK/C_Modules/Assignments/Assignment_0/que8.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main() {
+    int length = 10, width = 5;
+    int perimeter;
+
+    perimeter = 2 * (length + width);
+
+    printf("Length = %d\n", length);
+    printf("Width = %d\n", width);
+    printf("Perimeter = %d\n", perimeter);
+
+   
+}
