21bf303428b98e581522a470663673b3f12468b7
yashdeshmukh008-dev <yashdeshmukh008@gmail.com>
2026-07-06 16:49:39 +0530

    Create que7.c

---
+++ FBS_WORK/C_Modules/Assignments/Assignment_2/que7.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void main() {
+    int age = 25;
+
+    if (age < 12) {
+        printf("Child\n");
+    }
+    else if (age >= 12 && age <= 19) {
+        printf("Teenager\n");
+    }
+    else if (age >= 20 && age <= 59) {
+        printf("Adult\n");
+    }
+    else {
+        printf("Senior\n");
+    }
+
+    
+}
