87e38ed592810904471ed92105b3a28b6eeed24c
yashdeshmukh008-dev <yashdeshmukh008@gmail.com>
2026-07-06 16:36:12 +0530

    Create que3.c

---
+++ FBS_WORK/C_Modules/Assignments/Assignment_0/que3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main() {
+    float celsius = 25.0;
+    float fahrenheit;
+
+    fahrenheit = (celsius * 9 / 5) + 32;
+
+    printf("Celsius = %.2f\n", celsius);
+    printf("Fahrenheit = %.2f\n", fahrenheit);
+
+}
