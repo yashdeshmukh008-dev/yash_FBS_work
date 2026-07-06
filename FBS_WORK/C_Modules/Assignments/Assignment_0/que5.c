27ada5150f4e3b34f59920f82d8083041263ca55
yashdeshmukh008-dev <yashdeshmukh008@gmail.com>
2026-07-06 16:38:09 +0530

    Create que5.c

---
+++ FBS_WORK/C_Modules/Assignments/Assignment_0/que5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main() {
+    int n1 = 10, n2 = 20, n3 = 30, n4 = 40, n5 = 50;
+    float average;
+
+    average = (n1 + n2 + n3 + n4 + n5) / 5.0;
+
+    printf("Numbers: %d %d %d %d %d\n", n1, n2, n3, n4, n5);
+    printf("Average = %.2f\n", average);
+
+   
+}
