f36d76db0d3976bf4d3ed64fbc7fad87f82c728b
yashdeshmukh008-dev <yashdeshmukh008@gmail.com>
2026-07-06 16:39:08 +0530

    Create que6.c

---
+++ FBS_WORK/C_Modules/Assignments/Assignment_0/que6.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main() {
+    int num = 5;
+    int square, cube;
+
+    square = num * num;
+    cube = num * num * num;
+
+    printf("Number = %d\n", num);
+    printf("Square = %d\n", square);
+    printf("Cube = %d\n", cube);
+
+    
+}
