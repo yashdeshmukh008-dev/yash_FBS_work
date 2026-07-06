a1b35e0269a2ac8ba6890ff6319322534cd4c1b7
yashdeshmukh008-dev <yashdeshmukh008@gmail.com>
2026-07-06 16:41:59 +0530

    Create que9.c

---
+++ FBS_WORK/C_Modules/Assignments/Assignment_0/que9.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main() {
+    float base = 10.0, height = 8.0;
+    float area;
+
+    area = 0.5 * base * height;
+
+    printf("Base = %.2f\n", base);
+    printf("Height = %.2f\n", height);
+    printf("Area of Triangle = %.2f\n", area);
+
+    
+}
