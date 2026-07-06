91167799b38fd940d258a2a9ff1c6d5b4dd7956d
yashdeshmukh008-dev <yashdeshmukh008@gmail.com>
2026-07-06 16:34:13 +0530

    Create que2.c

---
+++ FBS_WORK/C_Modules/Assignments/Assignment_0/que2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main() {
+    float radius = 5.0;
+    float area;
+    const float PI = 3.14159;
+
+    area = PI * radius * radius;
+
+    printf("Radius = %.2f\n", radius);
+    printf("Area of the circle = %.2f\n", area);
+
+  
+}
