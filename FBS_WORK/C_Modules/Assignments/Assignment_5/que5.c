c1f6f1e68650cc8b6808fdf8706094dc92c03e46
yashdeshmukh008-dev <yashdeshmukh008@gmail.com>
2026-07-10 18:58:45 +0530

    Create que5.c

---
+++ FBS_WORK/C_Modules/Assignments/Assignment_5/que5.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void main() {
+    int n = 5;
+
+    for (int i = n; i >= 1; i--) {
+       
+        for (int j = 1; j <= n - i; j++) {
+            printf(" ");
+        }
+
+      
+        for (int j = 1; j <= (2 * i - 1); j++) {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+   
+}
