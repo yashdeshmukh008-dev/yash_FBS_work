b98d12310f0858932f3c26be87d3d497dac758f9
yashdeshmukh008-dev <yashdeshmukh008@gmail.com>
2026-07-10 19:24:47 +0530

    Create que8.c

---
+++ FBS_WORK/C_Modules/Assignments/Assignment_5/que8.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void main() {
+    int n = 4;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    for (int i = n - 1; i >= 1; i--) {
+        for (int j = 1; j <= i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+   
+}
