a265c0d432a8df9a92f88656b5fea14b9bcd73b2
yashdeshmukh008-dev <yashdeshmukh008@gmail.com>
2026-07-10 19:26:45 +0530

    Create que10.c

---
+++ FBS_WORK/C_Modules/Assignments/Assignment_5/que10.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void main() {
+    int n = 4;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (i == 1 || i == n || j == 1 || j == n || i==j)
+                printf("* ");
+            else
+                printf("  ");
+        }
+        printf("\n");
+    }
+
+   
+}
