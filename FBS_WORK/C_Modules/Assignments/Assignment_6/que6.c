4eb26543a46effdb2e77e48255033bb29380e6fd
yashdeshmukh008-dev <yashdeshmukh008@gmail.com>
2026-07-11 19:00:58 +0530

    Create que6.c

---
+++ FBS_WORK/C_Modules/Assignments/Assignment_6/que6.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void upper_lower(){
+  char ch = 'a';
+  if(ch>='a' && ch <='z')
+  printf("character is lower case");
+  else
+  printf("cracter is upper case");
+
+}
+void main(){
+  upper_lower();
+}
