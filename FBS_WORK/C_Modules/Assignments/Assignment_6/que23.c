cb1899b84b3c89e392fc3fabd72128ff7195ca37
yashdeshmukh008-dev <yashdeshmukh008@gmail.com>
2026-07-11 23:58:01 +0530

    Create que23.c

---
+++ FBS_WORK/C_Modules/Assignments/Assignment_6/que23.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void perfect(int n) {
+       
+    int i, j, sum;
+
+    printf("Perfect numbers between 1 and %d are:\n", n);
+
+    for (i = 1; i <= n; i++) {
+        sum = 0;
+
+       
+        for (j = 1; j <= i / 2; j++) {
+            if (i % j == 0)
+                sum += j;
+        }
+
+        if (sum == i)
+            printf("%d ", i);
+    }
+
+   
+}
+void main(){
+  int n = 1000;
+  perfect(n);
+}
