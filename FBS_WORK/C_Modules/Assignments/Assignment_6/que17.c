56de68bd50b6f5efeb0aa6456982a09fa2fea0f3
yashdeshmukh008-dev <yashdeshmukh008@gmail.com>
2026-07-11 19:52:03 +0530

    Create que17.c

---
+++ FBS_WORK/C_Modules/Assignments/Assignment_6/que17.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void prime(int a){
+  
+  int a = 2;
+  while(a<num){
+    if(num % a == 0)
+      printf("number is not prime");
+        else 
+        printf("number is prime");
+    a++;
+  }
+}
+void main(){
+  int num = 7;
+  prime(num);
+}
