64884fc3e171ea2d9276e0a6330d6f27fba9d0f9
yashdeshmukh008-dev <yashdeshmukh008@gmail.com>
2026-07-11 18:53:50 +0530

    Create que2.c

---
+++ FBS_WORK/C_Modules/Assignments/Assignment_6/que2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void pal(){
+      int a = 121;
+      int pal_num;
+      int x = a%10;
+      int y = a/10;
+      pal_num = (pal_num + x ) * 100;
+      x = y%10;
+      y = y /10;
+      pal_num = (pal_num + x ) * 10;
+      pal_num = (pal_num + y);
+  }
+void main(){
+  pal();
+}
