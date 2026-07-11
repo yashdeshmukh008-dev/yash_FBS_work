e485e0ff1f01389bca2dbd1d7f09bcd22b83f017
yashdeshmukh008-dev <yashdeshmukh008@gmail.com>
2026-07-11 19:29:08 +0530

    Create que11.c

---
+++ FBS_WORK/C_Modules/Assignments/Assignment_6/que11.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+void discount() {
+    float price = 650.0;
+    char student = 'y';   // 'y' = student, 'n' = not student
+
+    float discount = 0.0, finalPrice;
+
+    if (student == 'y' || student == 'Y') {
+        if (price > 500) {
+            discount = 0.20 * price;
+        } else {
+            discount = 0.10 * price;
+        }
+    } 
+    else if (student == 'n' || student == 'N') {
+        if (price > 600) {
+            discount = 0.15 * price;
+        } else {
+            discount = 0.0;
+        }
+    }
+
+    finalPrice = price - discount;
+
+    printf("Price = %.2f\n", price);
+    printf("Student = %c\n", student);
+    printf("Discount = %.2f\n", discount);
+    printf("Final Price = %.2f\n", finalPrice);
+
+   
+}
+void main(){
+  discount();
+}
