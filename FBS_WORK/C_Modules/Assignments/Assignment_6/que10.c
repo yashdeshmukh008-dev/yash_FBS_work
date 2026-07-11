941f63210bc50b3b6d18f4d6a90cdd7b4a4d185a
yashdeshmukh008-dev <yashdeshmukh008@gmail.com>
2026-07-11 19:26:53 +0530

    Create que10.c

---
+++ FBS_WORK/C_Modules/Assignments/Assignment_6/que10.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void result(){
+   int marks = 74;
+   if(marks >=75)
+   printf("distinction");
+   
+   else if(marks>=65)
+   printf("first class");
+       
+   else if(marks>=55)
+   printf("second class");
+
+   else if(marks>=40)
+   printf("pass class");
+
+   else if(marks<40)
+   printf("fail");
+
+  
+}
+void main(){
+  result();
+}
