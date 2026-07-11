01ae9e7c8e7c9d8606ed596fba8560465ac58d48
yashdeshmukh008-dev <yashdeshmukh008@gmail.com>
2026-07-11 18:59:10 +0530

    Create que4.c

---
+++ FBS_WORK/C_Modules/Assignments/Assignment_6/que4.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void vowel_check(){
+   char ch = s;
+   if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' )
+   printf("character is vowel");
+   else
+   printf("character is consonents");
+
+}
+void main(){
+  vowel_check();
+}
