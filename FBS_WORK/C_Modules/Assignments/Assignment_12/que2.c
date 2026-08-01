51c7ea0e85e950febf9786d4253b92f9c1678d7f
yashdeshmukh008-dev <yashdeshmukh008@gmail.com>
2026-08-01 19:28:58 +0530

    Create que2.c

---
+++ FBS_WORK/C_Modules/Assignments/Assignment_12/que2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    for(i = 0; str[i] != '\0'; i++) {
+        if(str[i] == 'a')
+            str[i] = '$';
+    }
+
+    printf("Modified String: %s", str);
+
+    return 0;
+}
