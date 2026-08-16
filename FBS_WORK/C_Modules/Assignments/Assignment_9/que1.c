612553a95e6b733007dfd1476235cb290aee19aa
yashdeshmukh008-dev <yashdeshmukh008@gmail.com>
2026-08-16 22:22:08 +0530

    Create que1.c

---
+++ FBS_WORK/C_Modules/Assignments/Assignment_9/que1.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void findMinMax(int arr[], int n)
+{
+    int min, max, i;
+
+    min = max = arr[0];
+
+    for(i = 1; i < n; i++)
+    {
+        if(arr[i] < min)
+            min = arr[i];
+
+        if(arr[i] > max)
+            max = arr[i];
+    }
+
+    printf("Minimum = %d\n", min);
+    printf("Maximum = %d\n", max);
+}
+
+int main()
+{
+    int arr[100], n, i;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter elements:\n");
+    for(i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    findMinMax(arr, n);
+
+    return 0;
+}
